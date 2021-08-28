411afcac77bed6b475382255032cee698d39879c
xyntechx <xyntechx@gmail.com>
2021-08-28 23:04:14 +0800

    🔖 Dr Ong

--- pages/dr-ong.js
+++ pages/dr-ong.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Unlock from "./components/unlock";
 
 export default function DrOng() {
     const unlock = async (event) => {
@@ -98,32 +99,7 @@ export default function DrOng() {
                 <br></br>
                 <br></br>
 
-                <form onSubmit={unlock}>
-                    <div className="mb-4">
-                        <label
-                            className="block text-gray-700 font-bold mb-2 text-xl"
-                            htmlFor="passcode"
-                        >
-                            The 4 letters from the 4 puzzles
-                        </label>
-                        <input
-                            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight border-blue-500 text-center text-xl focus:bg-blue-200 focus:outline-none"
-                            name="passcode"
-                            id="passcode"
-                            type="text"
-                            placeholder="XXXX"
-                            required
-                        ></input>
-                    </div>
-                    <div className="flex items-center justify-center">
-                        <button
-                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                            type="submit"
-                        >
-                            Unlock Ultimate Gift
-                        </button>
-                    </div>
-                </form>
+                <Unlock teacher="dr-ong"></Unlock>
             </main>
         </div>
     );
