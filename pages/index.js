aa82f301f76a6acb2ec7c065c5fdffc1996c1efc
xyntechx <xyntechx@gmail.com>
2021-08-28 13:44:49 +0800

    ✨ Home Page

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,82 @@
-import Head from 'next/head'
+import Head from "next/head"
 
 export default function Home() {
+  const registerUser = async event => {
+    event.preventDefault()
+
+    const res = await fetch("/api/hello", {
+      body: JSON.stringify({
+        loginid: event.target.loginid.value
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "POST"
+    })
+
+    // If you are reading this, I would like to thank you for taking the time to view the code for the website!
+    // However, please do NOT use the Login IDs which are not yours!
+    // I was too lazy to use environment variables 😂
+    const ID_TEACHERS = {
+      458585: "dr-ong",
+      892002: "mr-wong",
+      172391: "mr-ng",
+      374716: "ms-lin",
+      339540: "ms-avadhani",
+      675202: "mrs-tan",
+      144588: "ms-leong",
+      466877: "mr-iqbal",
+      778037: "cikgu-malek"
+    }
+
+    const IDS = Object.keys(ID_TEACHERS)
+
+    const result = await res.json()
+    IDS.includes(result.loginid) ? window.location.href = "/" + ID_TEACHERS[result.loginid] : alert("Please input a valid Login ID 👀")
+  }
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Happy Teachers' Day</title>
+        <meta name="description" content="Coded with ❤️ by Nyx" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
+          Happy Teachers' Day 🎉
         </h1>
 
         <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
+          Here's my gift to you 😊
         </p>
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+        <br></br>
+        <br></br>
 
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
+        <form onSubmit={registerUser}>
+          <div className="mb-4">
+            <label className="block text-gray-700 font-bold mb-2 text-xl" htmlFor="loginid">
+              Login ID
+            </label>
+            <input className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none border-blue-500" name="loginid" id="loginid" type="text" placeholder="******" required ></input>
+          </div>
+          <div className="flex items-center justify-center">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+              Login
+            </button>
+          </div>
+        </form>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
+        <br></br>
+        <br></br>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <p className="mt-3 text-xl">
+          Check out the code for this website (by yours truly) <a className="text-blue-600" href="https://github.com/xyntechx/Teachers-Day-2021">here</a>!
+        </p>
+
       </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
     </div>
   )
 }
