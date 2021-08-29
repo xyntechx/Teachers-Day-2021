ad9bd4aee24cb6bd5eb5416041a59f0ca48225fb
xyntechx <xyntechx@gmail.com>
2021-08-29 12:34:36 +0800

    🔖 Dr Ong

---
+++ pages/dr-ong/index.js
@@ -0,0 +1,17 @@
+import Teacher from "../components/teacher";
+
+export default function DrOng() {
+    return (
+        <Teacher
+            teacher={"Dr Ong"}
+            puzzleOne={"Solve this Math joke and get your first letter!"}
+            puzzleTwo={"Solve this trivia question and get your second letter!"}
+            puzzleThree={
+                "Identify your famous quotes and get your third letter!"
+            }
+            puzzleFour={
+                "Solve this other Math joke and get your fourth letter!"
+            }
+        ></Teacher>
+    );
+}
