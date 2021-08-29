95b92768e5458c04c5e4b9b72be00dfee33d3851
xyntechx <xyntechx@gmail.com>
2021-08-29 15:40:42 +0800

    ✨ Gift Custom Component

--- pages/components/gift.js
+++ pages/components/gift.js
@@ -1,21 +1,23 @@
 import Head from "next/head";
 
-export default function Gift({teacher, videoTitle}) {
+export default function Gift({ teacher, videoTitle }) {
     return (
         <div className="flex flex-col items-center justify-center min-w-full min-h-screen py-10">
             <Head>
-                <title>
-                    Happy Teachers' Day, {teacher}
-                </title>
+                <title>Happy Teachers' Day, {teacher}</title>
                 <meta name="description" content="Coded with ❤️ by Nyx" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <main className="flex flex-col items-center justify-center w-9/12 flex-1 text-center">
-                <h1 className="font-bold text-5xl md:text-6xl">Congratulations, {teacher} 🎉</h1>
+                <h1 className="font-bold text-5xl md:text-6xl">
+                    Congratulations, {teacher} 🎉
+                </h1>
 
                 <br></br>
 
-                <p className="mt-3 md:text-lg text-base">You have unlocked the Ultimate Gift (the video below)!</p>
+                <p className="mt-3 md:text-lg text-base">
+                    You have unlocked the Ultimate Gift (the video below)!
+                </p>
 
                 <br></br>
 
